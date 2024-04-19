66ed1fde8f2bb7eb0b448f36c1b9309117f6009e
unknown <saradha2125@gmail.com>
2024-04-18 22:24:24 -0400

    navigation

--- src/components/UserLogin/index.js
+++ src/components/UserLogin/index.js
@@ -17,7 +17,7 @@ const UserLogin = () => {
 			const url = "http://localhost:8080/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
-			window.location = "/";
+			window.location = "/ongoingtournament";
 		} catch (error) {
 			if (
 				error.response &&
